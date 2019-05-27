a546a91ee3d0d1cf6c2779a27ba114f0b17ed444
sornsama <sornsama@outlook.com>
2019-05-27 15:25:30 +0200

    periodic requests

---
+++ periodic_requests.py
@@ -0,0 +1,17 @@
+
+import requests, pytz
+from apscheduler.schedulers.twisted import TwistedScheduler
+from twisted.internet import reactor
+
+
+def send_requests():
+    requests.post("https://agile-scrubland-59074.herokuapp.com/schedule.json", data={
+        'project': 'default',
+        'spider': 'statistics'
+    })
+
+if __name__ == '__main__':
+    scheduler = TwistedScheduler(timezone=pytz.utc)
+    scheduler.add_job(send_requests, 'interval', hours=1, start_date='2019-27-5 15:30:00')
+    scheduler.start()
+    scheduler.run()
