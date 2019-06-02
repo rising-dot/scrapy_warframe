6181220de3138f91e4ae7e97624033fb31a6eb95
sornsama <sornsama@outlook.com>
2019-06-02 16:46:35 +0200

    initial commit

--- periodic_requests.py
+++ periodic_requests.py
@@ -3,25 +3,26 @@ from apscheduler.schedulers.twisted import TwistedScheduler
 from twisted.internet import reactor
 
 
-def send_requests():
+def send_request():
     requests.post("https://intense-dawn-47768.herokuapp.com/schedule.json", data={
         'project': 'default',
         'spider': 'statistics'
     })
 
+
 if __name__ == '__main__':
     scheduler = TwistedScheduler(timezone=pytz.utc)
-    scheduler.add_job(send_requests, 'cron', hour='*', jitter=120)
+    scheduler.add_job(send_request, 'interval',
+                      minutes=30, jitter=120)
+
     scheduler.start()
     reactor.run()
 
 
 
-
-
 # if __name__ == '__main__':
 #     scheduler = TwistedScheduler(timezone=pytz.utc)
-#     scheduler.add_job(send_requests, 'interval', hours=1, start_date='2019-06-02 09:45:00')
+#     scheduler.add_job(send_request, 'interval', hours=1, start_date='2019-06-02 09:45:00')
 #     scheduler.start()
 #     reactor.run()
 
