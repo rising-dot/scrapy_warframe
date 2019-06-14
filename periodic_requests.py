3a5dee4923709abb2b3c7afc8bad8f59a0b04d20
sornsama <sornsama@outlook.com>
2019-06-14 11:12:04 +0200

    initial commit

--- periodic_requests.py
+++ periodic_requests.py
@@ -12,7 +12,7 @@ def send_request():
 
 if __name__ == '__main__':
     scheduler = TwistedScheduler(timezone=pytz.utc)
-    scheduler.add_job(send_request, 'interval', hours=1, jitter=120)
+    scheduler.add_job(send_request, 'cron', hour='*', jitter=120)
 
     scheduler.start()
     reactor.run()
@@ -23,8 +23,9 @@ if __name__ == '__main__':
 #  'cron', day_of_week='mon-sun', hour='*', start_date='2019-06-13 14:00:00', jitter=120) --run 2 time only on a thusday
 #  'cron', hour='*', start_date='2019-06-13 21:00:00', jitter=120)                        --did not run
 #  'cron', day_of_week='thu-wed', hour='*', start_date='2019-06-13 22:00:00', jitter=120) --did not run
-#  'interval', hours=1, start_date='2019-06-14 22:45:00', jitter=120)                     --did not run
 
+#  'interval', hours=1, start_date='2019-06-14 22:45:00', jitter=120)                     --did not run
+#  'interval', hours=1, jitter=120)                                                       --did not run
 
 
 # 'cron', hour='*', jitter=120)
