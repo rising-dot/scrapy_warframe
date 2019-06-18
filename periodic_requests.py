bdafe3c141a2a2c28136b1d8d55830b5fea9fbd9
sornsama <sornsama@outlook.com>
2019-06-19 00:31:02 +0200

    initial commit

--- periodic_requests.py
+++ periodic_requests.py
@@ -12,7 +12,7 @@ def send_request():
 
 if __name__ == '__main__':
     scheduler = TwistedScheduler(timezone=pytz.utc)
-    scheduler.add_job(send_request, 'interval', minutes=59, start_date='2019-06-18 12:00:00', jitter=120)  # active before 30 min
+    scheduler.add_job(send_request, 'interval', minutes=59, start_date='2019-06-19 00:00:00', jitter=120)  # active before 30 min
 
     scheduler.start()
     reactor.run()
