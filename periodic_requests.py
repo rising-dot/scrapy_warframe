c4f8d369635352fde9a58774b916af358c4c13ab
sornsama <sornsama@outlook.com>
2019-12-19 02:24:23 +0100

    initial commit

--- periodic_requests.py
+++ periodic_requests.py
@@ -26,9 +26,7 @@ if __name__ == '__main__':
     ##################################      Run every hour       ##################################################
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=1-23, jitter=60)
     ########################################################################################
-
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0, minute=3, jitter=60)
-
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0, minute=30, jitter=60)
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=1, minute=30, jitter=60)
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=2, minute=30, jitter=60)
@@ -51,9 +49,9 @@ if __name__ == '__main__':
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=19, minute=30, jitter=60)
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=20, minute=30, jitter=60)
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=21, minute=30, jitter=60)
+    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=22, minute=30, jitter=60)
     scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=23, minute=30, jitter=60)
 
-    #scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=23, minute=57, jitter=60)
 
     scheduler.start()
     reactor.run()
