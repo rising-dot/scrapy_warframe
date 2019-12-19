eb5666776d13de4e65fefa68ceb97d892dbf44ad
sornsama <sornsama@outlook.com>
2019-12-19 10:26:37 +0100

    initial commit

--- periodic_requests.py
+++ periodic_requests.py
@@ -24,36 +24,49 @@ if __name__ == '__main__':
 
 
     ##################################      Run every hour       ##################################################
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=1-23, jitter=60)
+    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0-23, jitter=60)
     ########################################################################################
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0, minute=3, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=1, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=2, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=3, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=4, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=5, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=6, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=7, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=8, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=9, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=10, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=11, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=12, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=13, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=14, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=15, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=16, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=17, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=18, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=19, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=20, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=21, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=22, minute=30, jitter=60)
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=23, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0, minute=3, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=0, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=1, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=2, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=3, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=4, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=5, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=6, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=7, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=8, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=9, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=10, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=11, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=12, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=13, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=14, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=15, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=16, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=17, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=18, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=19, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=20, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=21, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=22, minute=30, jitter=60)
+    # scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour=23, minute=30, jitter=60)
 
 
     scheduler.start()
     reactor.run()
 
 
+#   https://apscheduler.readthedocs.io/en/latest/modules/triggers/combining.html
+
+
+# git init
+#
+# git add .
+#
+# git commit -m "initial commit"
+#
+# git push heroku master
+#
+#
+# heroku ps:scale clock=1
